d1f40188be35ab6d571929b8abb959212cb3e86c
Girisha <girishamunnangi@gmail.com>
2026-02-20 17:06:34 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
     egress {
         from_port = 0
         to_port = 0
-        protocol : "-1"
+        protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = [": : /0"]
     }
